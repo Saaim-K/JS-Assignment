e589e6e5c99f50da1a8f35fa02bad29606de440f
Saaim-K <saaimkhan221@gmail.com>
2022-05-26 01:10:49 +0500

    JS Assignment

--- app.js
+++ app.js
@@ -15,3 +15,4 @@ cancelBtn.onclick = ()=>{
 window.onscroll = ()=>{
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+// --------------------- Navbar JS ---------------------
